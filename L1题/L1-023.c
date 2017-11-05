3bf03c0af6f9825d0feb128b0ab499913b920c12
章鱼哥 <s969980719@vip.qq.com>
2017-11-05 19:32:45 +0800

    Update L1-023.c

--- L1题/L1-023.c
+++ L1题/L1-023.c
@@ -1,36 +1,59 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{
-	char A[10000],B[4]={'G','P','L','T'};
-	int ASCA[255],i;
-	gets(A);
-	for(i=0;i<strlen(B);i++)
-	{
-		ASCA[B[i]]=i;
-	}
-	for(i=0;i<strlen(A);i++)
-	{
-		if(ASCA[A[i]]==1)
-		{
-			printf("P");
-			break;
-		}
-		if(ASCA[A[i]]==0)
-		{
-			printf("G");
-			break;
-		}
-		if(ASCA[A[i]]==2)
-		{
-			printf("L");
-			break;
-		}
-		if(ASCA[A[i]]==3)
-		{
-			printf("T");
-			break;
-        }
-	}
-	return 0;
+{  
+  char C[10000]; 
+  int  sum[4]={0,0,0,0};  
+  int i=0,len=0; 
+  scanf("%s",C);
+  len=strlen(C);  
+  for(i=0;i<strlen(C);i++)
+  {    
+      switch(C[i])
+        {      
+      case 'G':
+        sum[0]++;break;      
+      case 'P':
+        sum[1]++;break;      
+      case 'L':
+        sum[2]++;break;          
+      case 'T':
+        sum[3]++;break;      
+      case 'g':
+        sum[0]++;break;      
+      case 'p':
+        sum[1]++;break;      
+      case 'l':
+        sum[2]++;break;      
+      case 't':
+        sum[3]++;break;
+    }
+  }  
+   while(sum[0]!=0||sum[1]!=0||sum[2]!=0||sum[3]!=0)
+   {    
+    for(i=0;i<4;i++)
+    {      
+      if(sum[i]!=0)
+      {
+          sum[i]--;        
+          if(i==0)
+          {
+          printf("G");
+          }       
+          if(i==1)
+          {
+          printf("P");
+          }        
+         if(i==2)
+         {
+          printf("L");
+         }        
+         if(i==3)
+         {
+          printf("T");
+         }
+     }
+    }
+  }  
+  return 0;
 }
